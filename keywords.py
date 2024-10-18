11a215b2e1a3a1948ae4474ea83b9862d1cfd241
Duhamster456 <Vokshinerg@gmail.com>
2024-10-18 17:12:12 +0300

    keywords search functions

---
+++ keywords.py
@@ -0,0 +1,53 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pymorphy3
+import re
+
+_g_key_words_amount = 6
+_g_morph = pymorphy3.MorphAnalyzer()
+
+def to_key_words(questions : list[list[str]]) -> list[list[str]]:
+    questions = lemmatise_questions(questions)
+    all_kwords = get_all_keywords(questions, _g_key_words_amount)
+    for question in questions:
+        for i in range(len(question)):
+            this_answer_kwords = []
+            for word in question[i]:
+                if word in all_kwords:
+                    this_answer_kwords.append(word)
+            question[i] = this_answer_kwords
+    return questions
+
+def get_all_keywords(questions : list[list[str]], amount) -> set[str]:
+    all_answers = []
+    for question in questions:
+        all_answers.extend(question)
+    all_words = []
+    for word in all_answers:
+        all_words.extend(word)
+    vectorizer = TfidfVectorizer()
+    vector = vectorizer.fit_transform(all_words)
+    feature_names = vectorizer.get_feature_names_out()
+    keywords = [feature_names[i] for i in vector.toarray().argsort()[0][-amount:]] 
+    return set(keywords)
+
+def lemmatize_string(text : str) -> list[str]:
+    words = re.findall(r'\w+', text)
+    res = list()
+    for word in words:
+        p = _g_morph.parse(word)[0]
+        res.append(p.normal_form)
+    return res
+
+def lemmatise_questions(questions : list[list[str]]) -> list[list[str]]:
+    for question in questions:
+        for i in range(len(question)):
+            question[i] = lemmatize_string(question[i])
+    return questions
+
+
+def main():
+    questions = [["привет, я миша", "привет, я ваня", "привет, я олеся"], ["вставили текст", "вставить", "я жак Фреско, привет!"], ["ра ра ра цы цы цы ыыыыыыыыыыыыыыыыыы"]]
+    print(to_key_words(questions))
+
+if __name__ == "__main__":
+    main()
