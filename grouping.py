f55d728c60181ace05fc458491820bbe4149c677
Duhamster456 <Vokshinerg@gmail.com>
2024-10-18 21:32:50 +0300

    now grouping into 5 dictionaries

--- grouping.py
+++ grouping.py
@@ -1,12 +1,14 @@
-def group_keywords(data_to_group : list[list[str]], grouped_keywords : dict[str, set[str]]) -> dict[str, int]:
-    result = dict()
+def group_keywords(data_to_group : list[list[str]], grouped_keywords : dict[str, set[str]]) -> list[dict[str, int]]:
+    result = list()
     for question in data_to_group:
+        result_for_question = dict()
         for answer in question:
             for word in answer:
                 for group_name in grouped_keywords:
                     if(word == group_name or word in grouped_keywords[group_name]):
-                        if(word in result):
-                            result[word] += 1
+                        if(word in result_for_question):
+                            result_for_question[word] += 1
                         else:
-                            result[word] = 0
+                            result_for_question[word] = 0
+        result.append(result_for_question)
     return result
