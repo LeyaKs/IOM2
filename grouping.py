6fb7674353c732553c8e0b2e552cb028248ab02c
Duhamster456 <Vokshinerg@gmail.com>
2024-10-18 21:43:34 +0300

    error fixed

--- grouping.py
+++ grouping.py
@@ -6,9 +6,9 @@ def group_keywords(data_to_group : list[list[str]], grouped_keywords : dict[str,
             for word in answer:
                 for group_name in grouped_keywords:
                     if(word == group_name or word in grouped_keywords[group_name]):
-                        if(word in result_for_question):
-                            result_for_question[word] += 1
+                        if(group_name in result_for_question):
+                            result_for_question[group_name] += 1
                         else:
-                            result_for_question[word] = 0
+                            result_for_question[group_name] = 0
         result.append(result_for_question)
     return result
