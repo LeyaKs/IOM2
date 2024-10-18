bd24d06d83d33e9da1b861a04519a6ab6b1437c1
Duhamster456 <Vokshinerg@gmail.com>
2024-10-18 22:13:33 +0300

    everything united in one function

---
+++ data_grouped.py
@@ -0,0 +1,14 @@
+from keywords import to_key_words
+from parsing import parsing
+from preproccess import preprocess
+from translator import keywords_translate
+from groups_parse import parse_groups_file
+from grouping import group_keywords
+
+def group(filename : str) -> list[dict[str, int]]:
+    parsing_res = parsing("test.xlsx")
+    prep_res = preprocess(parsing_res)
+    keywords_res = to_key_words(prep_res)
+    groups_parsed = parse_groups_file("./resources/group.txt")
+    result = group_keywords(keywords_res, groups_parsed)
+    return result
