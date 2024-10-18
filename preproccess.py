4383c32478dab1afc88dbc7a613673e81403667b
Hachen-W <97247091+Hachen-W@users.noreply.github.com>
2024-10-18 18:22:50 +0300

    remove test

--- preproccess.py
+++ preproccess.py
@@ -1,18 +1,3 @@
-import openpyxl
-def parsing(file_name):
-    wb = openpyxl.load_workbook(file_name)
-    result = []
-    index = 0
-    for sheet in wb:        #Проход по страницам excel файла
-        answers = []        #Список ответов для каждого опрошенного
-        check_first_row = 1 #Флаг проверки первой строки 
-        for row in sheet.iter_rows(values_only=True):   #Проход по строкам на каждой странице
-            if (check_first_row == 1):
-                check_first_row = 0
-                continue
-            answers.append(list(row))   #Добавление ответа в список
-        result.append(answers)  #Добавление ответов в результирующий список
-    return result
 '''
 prepross for user and hr
 '''
@@ -114,8 +99,3 @@ def string_purify(string):
     for symbols_delete in USELESS_SYMBOLS:
         string = string.replace(symbols_delete, '')
     return string
-
-array = preprocess(parsing('task.xlsx'))
-for i in range(50):
-    print(array[0][i])
-    print()
