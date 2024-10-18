b30ffb2302a46dbe2b486fb1d744d4398b45f398
LeyaKs <olesyakashirina93@mail.ru>
2024-10-18 17:33:51 +0300

    Add 2 html and docker

---
+++ hackatom/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_file, render_template
+import os
+
+HDRS = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n'
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+   return render_template('index.html')
+
+def init_dir(filename):
+  if not os.path.exists(filename):
+    os.makedirs(filename)
+
+if __name__ == '__main__':
+  init_dir('uploads')
+  app.run(debug=True)
