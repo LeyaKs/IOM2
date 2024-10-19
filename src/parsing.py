480c4beace25c5b81f01f6a4983c3c724cfa3d50
Duhamster456 <Vokshinerg@gmail.com>
2024-10-19 04:25:41 +0300

    comments translated

--- src/parsing.py
+++ src/parsing.py
@@ -3,13 +3,13 @@ def parsing(file_name):
     wb = openpyxl.load_workbook(file_name)
     result = []
     index = 0
-    for sheet in wb:        #Проход по страницам excel файла
-        answers = []        #Список ответов для каждого опрошенного
-        check_first_row = 1 #Флаг проверки первой строки 
-        for row in sheet.iter_rows(values_only=True):   #Проход по строкам на каждой странице
+    for sheet in wb:        #Iterating through pages of the file
+        answers = []        #List of answers for each interviewee
+        check_first_row = 1 #flag variable
+        for row in sheet.iter_rows(values_only=True):   #Iteraring through rows on each page
             if (check_first_row == 1):
                 check_first_row = 0
                 continue
-            answers.append(list(row))   #Добавление ответа в список
-        result.append(answers)  #Добавление ответов в результирующий список
+            answers.append(list(row))   #adding an answer to the list
+        result.append(answers)  #adding answers to the resulting list
     return result
