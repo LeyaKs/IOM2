279bf93309fb1e3b9b5d7a5ac4586c337e39c9d8
LeyaKs <olesyakashirina93@mail.ru>
2024-10-19 10:09:04 +0300

    fix style and readme

--- src/preproccess.py
+++ src/preproccess.py
@@ -10,12 +10,14 @@ def preprocess(array):
         if i != 4:
             length_user = len(array_user)
             for j in range(length_user):
-                if array_user[j][i] != '' or any(word in array_user[j][i] for word in USELESS_ANSWERS) and i == 0:
+                if array_user[j][i] != '' or any(word in array_user[j][i]
+                 for word in USELESS_ANSWERS) and i == 0:
                     array_ans[i].append(array_user[j][i])
             
         length_hr = len(array_hr)
         for j in range(length_hr):
-            if array_hr[j][i] != '' or any(word in array_hr[j][i] for word in USELESS_ANSWERS) and i == 0:
+            if array_hr[j][i] != '' or any(word in array_hr[j][i] 
+            for word in USELESS_ANSWERS) and i == 0:
                 array_ans[i].append(array_hr[j][i])
             
     return array_ans
@@ -33,11 +35,12 @@ def preprocess_user(array):
     lines_count = len(array)
     columns_count = len(array[0]) if len(array) > 0 else 0
     for line in range(lines_count):
-        # объединение 1 и 1.1 вопросов
+        # merging 1 and 1.1 questions
         array[line][0] = ' '.join([array[line][0], array[line][1]])
         array[line].pop(1)
-        # обработка каждой строки
-        array[line] = [user_line_purify(str(string), column, string_dlc) for string, column, string_dlc in string_unite(array[line], range(4))]
+        # processing each line
+        array[line] = [user_line_purify(str(string), column, string_dlc) 
+        for string, column, string_dlc in string_unite(array[line], range(4))]
     return array
 
 '''
@@ -55,12 +58,11 @@ def string_unite(array_questions, array_numbers):
     return array_unite
 
 def user_line_purify(string, column, string_dlc = ''):
-    if column == QUESTION_STAY or column == QUESTION_RETURN:            # дублирование: номера вопросов, в которых может быть фраза 'описано выше'
-        if 'описано выше' in string:
+    if column == QUESTION_STAY or column == QUESTION_RETURN:  # duplication: numbers of questions in which the phrase can be described earlier
             string = string_dlc
     if len(string) < LEN_ACCESS:
         string = str(string)
-        for word in string.split():                                     # проверка на мусор
+        for word in string.split():                                # checking for trash
             if word in USER_SPECIAL_WORDS:
                 string = ''
                 break
@@ -75,7 +77,7 @@ def preprocess_hr(array):
     lines_count = len(array)
     columns_count = len(array[0]) if len(array) > 0 else 0
     for line in range(lines_count):
-        # добавление 1 ответа
+        # adding 1 question
         if any([word in array[line][3] for word in HR_SPECIAL_WORDS]):
             array[line].insert(0, 'желание сменить направление дейстельности')
         else:
@@ -93,7 +95,8 @@ def array_purify(array):
         array[line] = [string_purify(string) for string in array[line]]
     return array
 
-USELESS_SYMBOLS = ['!', '@', '"', '#', '№', '$', ';', '%', '^', ':', '&', '?', '*', '(', ')', '-', '_', '+', '=', '\\', '|', '/', '\'', '[', ']', '{', '}', '.', '<', '>']
+USELESS_SYMBOLS = ['!', '@', '"', '#', '№', '$', ';', '%', '^', ':', 
+'&', '?', '*', '(', ')', '-', '_', '+', '=', '\\', '|', '/', '\'', '[', ']', '{', '}', '.', '<', '>']
 def string_purify(string):
     string = str(string).lower()
     for symbols_delete in USELESS_SYMBOLS:
