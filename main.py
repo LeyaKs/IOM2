a1bace869e58f8aad1b772b4a8d9f9a9e87f51b8
LeyaKs <olesyakashirina93@mail.ru>
2024-10-19 03:22:37 +0300

    add workers, personal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file, render_template
 import os
 import json
+from data_grouped import group
 
 HDRS = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n'
 app = Flask(__name__)
@@ -14,7 +15,8 @@ def upload_file():
     input_file_path = os.path.join('uploads', file.filename)
     file.save(input_file_path)
 
-    array = [{'A':24, 'B':64}, {'A1':25, 'B1':64}]  
+    array = group(file)
+    print(array)
     rep(array)
     # Передача данных в JSON-формате в HTML
     #chart_data_json = json.dumps(chart_data)
@@ -27,42 +29,44 @@ def upload_file():
 
 
 def rep(array):
+  workers = ("Работа", "Зарплата", "Менеджмент", "Возможности роста", 
+  "Атмосфера", "Перегрузка", "Условия труда", "Стабильность", "Честность",
+  "Условия конкурентов привлекательнее", "Негативные эмоции")
+  personal = ("Переезд", "Семья", "Обучение", "Здоровье", "Ценности", "Приоритеты", "Неудовлетворение")
   ind = 1
   with open("templates/report_sample.html", 'r+', encoding='utf-8') as infile, open("templates/report.html", 'w', encoding='utf-8') as outfile:
     for line in infile:
       
       keys = list(array[ind-1].keys())
       values = list(array[ind-1].values())
-      str_keys = ""
+      str_keys = "\""
       str_values = ""
-      switch(ind):
+      match(ind):
         case 2:
-          for index in range(0,11):
-            str_keys += keys[index]
-            if key != keys[10]:
+          for index in range(len(keys)):
+            if keys[index] in personal:
+              str_keys += keys[index]
               str_keys += '" , "'
-            str_values += values[index]
-            if value != values[10]:
-              str_values += ' , '  
-          break;
-        case 3:
-          for index in range(11,18):
-            str_keys += keys[index]
-            if key != keys[17]:
+              str_values += str(values[index])
+              str_values += ' , ' 
+        case 3:  
+          for index in range(len(keys)):
+            if keys[index] in workers:
+              str_keys += keys[index]
               str_keys += '" , "'
-            str_values += values[index]
-             if value != values[17]:
-              str_values += ' , '  
-          break;
-        default:
-          for key in keys:
-            str_keys += key
-            if key != keys[-1]:
+              str_values += str(values[index])
+              str_values += ' , '     
+        case _:
+          for index in range(len(keys)):
+            if keys[index] in workers or keys[index] in personal:
+              str_keys += keys[index]
               str_keys += '" , "'
-          for value in values:
-            str_values += str(value)
-            if value != values[-1]:
-              str_values += ' , '    
+              str_values += str(values[index])
+              str_values += ' , '   
+      str_keys = str_keys[:-3]
+      str_values = str_values[:-3]
+      print(ind)
+      print(len(array))
       line = line.replace(f'@k{ind}', str_keys)
       line = line.replace(f'@v{ind}', str_values)
       if str_values in line and ind != len(array):
